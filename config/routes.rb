58508c8e64af55665647eac05f0b8408d6d23584
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-08 21:51:23 +0900

    パスワードチェック用のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'lounges#index'
-  resources :lounges, only: [:index, :new, :create, :show]
+  resources :lounges, only: [:index, :new, :create, :show] do
+    get 'password/request', to: 'lounges#password_request'
+    post 'password/check', to: 'louges_rooms#check'
+  end
 end
