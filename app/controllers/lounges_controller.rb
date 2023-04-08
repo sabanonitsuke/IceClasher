644aa9eab75649795283e11cd69369fdaa4855bf
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-08 23:20:27 +0900

    作成者特権で自分のラウンジには入れる

--- app/controllers/lounges_controller.rb
+++ app/controllers/lounges_controller.rb
@@ -1,5 +1,7 @@
 class LoungesController < ApplicationController
   before_action :push_sign_in, only: [:new, :create]
+  before_action :get_lounge, only: :show
+  before_action :author_privileges, only: :show
   before_action :require_valid_password, only: :show
   before_action :check_password, only: :check
 
@@ -21,7 +23,6 @@ class LoungesController < ApplicationController
   end
 
   def show
-    @lounge = Lounge.find(params[:id])
   end
 
   def password_request
@@ -43,12 +44,22 @@ class LoungesController < ApplicationController
     redirect_to new_user_session_path unless user_signed_in?
   end
 
+  def get_lounge
+    @lounge = Lounge.find(params[:id])
+  end
+
+
+  # あいことば関連のアクション
+
+  def author_privileges
+    render :show if user_signed_in? && current_user.id == @lounge.user.id
+  end
+
   def check_password
     password = params[:password]
     lounge = Lounge.find(params[:lounge_id])
-    binding.pry
     unless password && lounge.authenticate(params[:password])
-      flash[:alert] = 'パスワードが間違っています。'
+      flash[:alert] = 'あいことば が違います'
       redirect_to lounge_password_request_path(params[:lounge_id])
     end
   end
