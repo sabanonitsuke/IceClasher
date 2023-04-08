8bd846c719ced6fb382446af2eaad7150cf54ecd
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-08 17:24:57 +0900

    showルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'lounges#index'
-  resources :lounges, only: [:index, :new, :create]
+  resources :lounges, only: [:index, :new, :create, :show]
 end
