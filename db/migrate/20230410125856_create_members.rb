aaf0377670fe9eeb1d533db6cffd0a72ca43f104
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-10 22:05:03 +0900

    membersテーブル作成

--- db/migrate/20230410125856_create_members.rb
+++ db/migrate/20230410125856_create_members.rb
@@ -1,7 +1,9 @@
 class CreateMembers < ActiveRecord::Migration[6.0]
   def change
     create_table :members do |t|
-
+      t.references :lounge, null: false, foreign_key: true
+      t.string :name, null: false
+      t.boolean :attendance, null: false
       t.timestamps
     end
   end
