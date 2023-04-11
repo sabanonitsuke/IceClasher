7f8ebd49fdfe017f3adb37612fdc8cf5d581ecad
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-11 15:25:50 +0900

    membersコントローラーの設定

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,11 @@
 class MembersController < ApplicationController
   def create
-    
+    member = Member.create(members_params)
+    render json:{ member: member }
+  end
+
+  private
+  def members_params
+    params.require(:member).permit(:name).merge(attendance: :ture, louge_id: params[:lounge_id])
   end
 end
