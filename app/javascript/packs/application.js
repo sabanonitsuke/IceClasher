b55e4f2368116821d6d562744a22bbad8ec2ffcd
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-12 21:57:41 +0900

    tipic.jsをtimer.jsに合体

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,6 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../topic")
 require("../timer")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/javascript/timer.js
+++ app/javascript/timer.js
@@ -6,8 +6,11 @@ function timer() {
   const topicDisplay = document.getElementById('topic-display');
   const people = document.getElementById('people');
   const memberSubmit = document.getElementById('member-submit');
+  const topicSubmit = document.getElementById('topic-submit');
+
   updateMembers();
   updateMemberItems();
+  updateTopicItems();
   updatePeople();
 
   function updateMembers() {
@@ -41,6 +44,31 @@ function timer() {
     });
   }
 
+  function updateTopicItems() {
+    const topicDelBtns = document.querySelectorAll(".topic-del-btn");
+    topicDelBtns.forEach(function (topicDelBtn) {
+      if (topicDelBtn.dataset.listenerAdded !== 'true') {
+        topicDelBtn.dataset.listenerAdded = 'true'
+        topicDelBtn.addEventListener('click', function () {
+          const loungeId = topicDelBtn.getAttribute("data-lounge_id");
+          const topicId = topicDelBtn.getAttribute("data-topic_id");
+          const XHR = new XMLHttpRequest();
+          XHR.open("DELETE", `/lounges/${loungeId}/topics/${topicId}`, true);
+          const token = document.querySelector('meta[name="csrf-token"]').content;
+          XHR.setRequestHeader('X-CSRF-Token', token);
+          XHR.send();
+          XHR.onload = () => {
+            if (XHR.status === 204) {
+              topicDelBtn.parentElement.remove();
+            } else {
+              alert(`Error ${XHR.status}: ${XHR.statusText}`);
+            };
+          };
+        });
+      }
+    });
+  }
+
   function updatePeople() {
     people.innerHTML = members.length
   };
@@ -52,10 +80,6 @@ function timer() {
     updateMemberItems();
     updatePeople();
   };
-
-  resetBtn.addEventListener('click', function () {
-    reset();
-  });
 
   memberSubmit.addEventListener('click', function (e) {
     e.preventDefault();
@@ -89,6 +113,43 @@ function timer() {
       updateMemberItems();
       updateMembers();
     };
+  });
+
+  topicSubmit.addEventListener('click', function (e) {
+    e.preventDefault();
+    const topicForm = document.getElementById('topic-form');
+    const formData = new FormData(topicForm);
+    if (formData.get('name') === "") {
+      alert(`値を入力してください`);
+      return null;
+    };
+    const XHR = new XMLHttpRequest();
+    const loungeId = topicForm.dataset.loungeId;
+    XHR.open("POST", `/lounges/${loungeId}/topics`, true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const formText = document.getElementById('topic-text');
+      const list = document.getElementById("topic-list");
+      const item = XHR.response.topic;
+      const html = `
+      <li class="content-list-item">
+        ${item.name}
+        <i class="topic-del-btn del-btn fa-solid fa-xmark", data-topic_id= "${item.id}", data-lounge_id = "${loungeId}" ></i>
+      </li>
+      `;
+      list.insertAdjacentHTML("beforeend", html);
+      formText.value = ""
+      updateTopicItems();
+    };
+  });
+
+  resetBtn.addEventListener('click', function () {
+    reset();
   });
 
   nextBtn.addEventListener('click', function () {
